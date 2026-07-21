c2c5ffb41ecfee3a7da7ecac88fb3098e9b3aae3
tmidgley <travis.midgley@gmail.com>
2026-07-21 00:07:33 -0700

    w04 interview question

---
+++ W04InterviewQuestion.cs
@@ -0,0 +1,37 @@
+// STACK (Last In, First Out)
+
+// Push
+// Add a new node to the front (Head)
+// O(1)
+
+// Pop
+// Remove the node at the front (Head)
+// O(1)
+
+// GetTop
+// Look at the Head node
+// O(1)
+
+// IsEmpty
+// Check if Head is null
+// O(1)
+
+
+
+// QUEUE (First In, First Out)
+
+// Enqueue
+// Add a new node to the end (Tail)
+// O(1)
+
+// Dequeue
+// Remove the node at the front (Head)
+// O(1)
+
+// Size
+// Return the count variable
+// O(1)
+
+// IsEmpty
+// Check if Head is null
+// O(1)
